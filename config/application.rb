a9df9a2e912f349174dc08eea34ce540c0d3fb06
jiteshsharma2 <jitesh.sharma2@tcs.com>
2017-11-05 23:58:02 +0530

    Adding confirmable module to user

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,15 @@ module DeviseProject
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActionMailer::Base.smtp_settings = {
+      :address => 'smtp.gmail.com',
+      :domain => 'mail.google.com',
+      :port => 587,
+      :user_name => 'adef3405@gmail.com',
+      :password => 'Jitesh1.',
+      :authentication => 'login',
+      :enable_starttls_auto => true
+    }
   end
 end
